41c8632016e436388931d08a44085a8d31772d7d
Michael Lee <michaelleemichaellee408@gmail.com>
2023-04-24 17:08:14 +0100

    Added new functions

--- src/functions/helpers.php
+++ src/functions/helpers.php
@@ -14,3 +14,25 @@ function __get_option( string $option ) {
     $options = get_option( CONFIG['PLUGIN_OPTION'] );
     return $options[ $option ] ?? false;
 }
+
+if ( ! function_exists( 'rocket_direct_filesystem' ) ) {
+    /**
+     * Instanciate the filesystem class
+     *
+     * @return WP_Filesystem_Direct WP_Filesystem_Direct instance
+     */
+    function rocket_direct_filesystem() {
+        require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php';
+        require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-direct.php';
+        return new WP_Filesystem_Direct( new StdClass() );
+    }
+}
+
+/**
+ * Check if WP Rocket is active.
+ *
+ * @return boolean
+ */
+function is_wpr_active() : bool {
+    return defined( 'WP_ROCKET_VERSION' );
+}
