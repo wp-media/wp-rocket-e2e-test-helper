ff0981c2b5cf22c36df0ace30c5f97458e58ee02
Michael Lee <michaelleemichaellee408@gmail.com>
2023-04-27 16:46:31 +0100

    Show custom notice when wpr is not active

--- views/main.php
+++ views/main.php
@@ -3,12 +3,19 @@ defined( 'ABSPATH' ) || exit;
 
 settings_errors( CONFIG['PLUGIN_ID'] . '_response' );
 require_once 'templates/header.php'; 
+
+if ( ! is_wpr_active() ) :
 ?>
-        <?php foreach ( $this->views as $view ) : ?>
-            <div class="tab-pane fade<?php echo $view === current( $this->views ) ? ' show active' : ''; ?>" id="<?php echo esc_attr( $view['pane'] ); ?>" role="tabpanel" aria-labelledby="<?php echo esc_attr( $view['id'] ); ?>_tab" tabindex="0">
-                <div class="container-fluid py-2">
-                    <?php $this->template->load_view( $view['id'] ); ?>
-                </div>
+    <div class="container-fluid">
+        <div class="alert alert-warning py-2">
+        <small><strong>WP Rocket is currently not active. The E2E test helper plugin might not work correctly.</strong></small>
+        </div>
+    </div>
+    <?php endif; foreach ( $this->views as $view ) : ?>
+        <div class="tab-pane fade<?php echo $view === current( $this->views ) ? ' show active' : ''; ?>" id="<?php echo esc_attr( $view['pane'] ); ?>" role="tabpanel" aria-labelledby="<?php echo esc_attr( $view['id'] ); ?>_tab" tabindex="0">
+            <div class="container-fluid py-2">
+                <?php $this->template->load_view( $view['id'] ); ?>
             </div>
-        <?php endforeach ?>
+        </div>
+    <?php endforeach ?>
 <?php require_once 'templates/footer.php'; ?>
