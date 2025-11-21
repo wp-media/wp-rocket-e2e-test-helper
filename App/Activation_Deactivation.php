fd1e1b29eba068dccd1dae67d51b17b410192190
Mai <mai@wp-media.me>
2025-11-21 11:46:53 +0200

    set default value of RI to false with activation

--- App/Activation_Deactivation.php
+++ App/Activation_Deactivation.php
@@ -15,7 +15,7 @@ class Activation_Deactivation {
         $config = [
             'rocket_post_purge_urls' => 'default',
             'rocket_exclude_post_taxonomy' => 'default',
-            'rocket_rocket_insights_enabled' => '',
+            'rocket_rocket_insights_enabled' => 'false_return',
         ];
 
         add_option( CONFIG['PLUGIN_OPTION'], $config );
