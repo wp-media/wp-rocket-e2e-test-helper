4e57efcafb777cc3be02ce61a32db82e3b09ef73
Mai <mai@wp-media.me>
2026-04-24 15:10:27 +0300

    added license sample

---
+++ config/license-credentials.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'single' => [
+        'key' => '',
+        'email' => '',
+    ],
+    'plus' => [
+        'key' => '',
+        'email' => '',
+    ],
+    'multi_50' => [
+        'key' => '',
+        'email' => '',
+    ],
+    'multi_100' => [
+        'key' => '',
+        'email' => '',
+    ],
+    'multi_500' => [
+        'key' => '',
+        'email' => '',
+    ],
+];
